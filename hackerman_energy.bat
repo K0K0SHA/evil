85d3308db6cdd6758436f1637a21568493841be0
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-11-07 03:44:44 -0500

    f11 to go fullscreen
    Need to update CMD to go fullscreen upon running batch script

--- hackerman_energy.bat
+++ hackerman_energy.bat
@@ -7,5 +7,5 @@ cd /
 goto a
 
 REM // # hackerman_energy.bat is a colorful way to perform a minor stress test on your Windows machine.
-REM // # think of this program as a screensaver, but cooler. And run it in a fullscreen CMD prompt.
+REM // # think of this program as a screensaver, but cooler. And run it in a fullscreen CMD prompt(F11).
 REM // # 
